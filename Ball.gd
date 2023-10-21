9dda9805e1bed535d392b49ae938ed0a8b1ff666
Tyler Morgan <tylermorgan86@gmail.com>
2023-10-20 23:31:34 -0700

    ball moving and bouncing

--- Ball.gd
+++ Ball.gd
@@ -1,3 +1,9 @@
 extends CharacterBody2D
 
+func _ready():
+	velocity = Vector2(-100, 100)
+
 func _physics_process(delta):
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+		velocity = velocity.bounce(collision_info.get_normal())
